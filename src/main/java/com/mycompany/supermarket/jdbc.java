208a2152e965d47b766a47f8885b954f74974e63
amro <jomaamro97@gmail.com>
2025-04-15 22:10:05 +0300

    Add files via upload

---
+++ src/main/java/com/mycompany/supermarket/jdbc.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.supermarket;
+
+/**
+ *
+ * @author kamal
+ */
+public class jdbc {
+        public static void main (String[] args){
+            System.out.println("HelloWord");
+       //     new Product().setVisible(true);
+         //   new Category().setVisible(true);
+         new gui().setVisible(true);
+        // gui().dispose();
+         
+            
+            
+            
+            
+        }
+        
+    
+}
